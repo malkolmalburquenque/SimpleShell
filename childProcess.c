412e9858a96a29d92f4159596a7e069aa5fe55bf
Malkolm Alburquenque <malkolm.alburquenque@mail.mcgill.ca>
2017-01-28 13:38:00 -0500

    working on first part

--- childProcess.c
+++ childProcess.c
@@ -3,15 +3,23 @@
 #include <string.h>
 #include <stdlib.h>
 
+//this method empties args
+void emptyArgs(char *args[]){
+	for (size_t i = 0; i < 20; i++) {
+		args[i]=NULL;
+	}
+}
+
 int getcmd(char *prompt, char *args[], int *background){
-	int length i=0;
+	int length, i=0;
 	char *token, *loc;
 	char *line = NULL;
 	size_t linecap = 0;
+	emptyArgs(args);
 
 	printf("%s", prompt);
 	length = getline(&line, &linecap, stdin);
-	
+
 	if (length <= 0) {
 		exit(-1);
 	}
@@ -23,10 +31,10 @@ int getcmd(char *prompt, char *args[], int *background){
 		*background = 0;
 
 	while ((token = strsep(&line, " \t\n")) != NULL){
-		for (int j=0; j<strnlen(token); j++)
+		for (int j=0; j<strlen(token); j++)
 			if (token[j] <=32)
 				token[j] = '\0';
-		if (strnlen(token) > 0)
+		if (strlen(token) > 0)
 			args[i++] = token;
 	}
 
@@ -39,7 +47,27 @@ int main (void){
 
 	while (1) {
 		bg =0;
-		int cnt = getcmd("\n>> ", args, &bg);
+		int cnt = getcmd("\n8===natelikespenis===> ", args, &bg);
 
+        /* the steps can be..:
+         * (1) fork a child process using fork()
+         * (2) the child process will invoke execvp()
+         * (3) if background is not specified, the parent will wait,
+         * otherwise parent starts the next command... */
+
+       int pid = fork();
+        if (pid == 0) {
+            execvp(args[0], args);
+		}
+		else{
+			wait(2);
+			if (bg == 1){
+				waitpid(pid);
+			}
+			else{
+				wait(2);
+				printf("hello i am ur daddy\n");
+			}
+		}
 	}
 }
